1905d68433fd864bad8894cf06d2d66154044672
Noe Perez <nperez@icstars.org>
2022-08-08 18:04:26 -0500

    23 minutes in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 const api = {
   key: "355d630f4d4d938f0b80f8f52015103a",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if(evt.key ==="Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric%APPID=${api.key}`)
+        .then(res => res.json())
+        .then (result => setWeather(result));
+    }
+  }
   
     const dateBuilder = (d) => {
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
